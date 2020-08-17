862c959ba5f74a085614a1cd7e7c55866cd38032
Дмитрий <>
2020-08-17 10:27:26 +0300

    update

--- org1node.sh
+++ org1node.sh
@@ -37,7 +37,9 @@ sudo scp $USER@$ADMIN_IP:/etc/hyperledger/configtx/Org1MSPanchors.tx /etc/hyperl
 
 mv /etc/hyperledger/fabric/core_org1.yaml /etc/hyperledger/fabric/core.yaml 
 
-cp orderer-config/core_org1.yaml /etc/hyperledger/fabric/core.yaml 
+#cp orderer-config/core_org1.yaml /etc/hyperledger/fabric/core.yaml 
+
+git clone https://github.com/kykini/hlf_2.2_docker_free.git
 
 cp -r hlf_2.2_docker_free/external-builder/ /etc/hyperledger/
 chmod -R 777 /etc/hyperledger/external-builder/
@@ -85,5 +87,17 @@ peer channel update -o orderer.hypertest.com:7050 -c hypertest -f /etc/hyperledg
 
 peer channel fetch 0 hypertest.block -c hypertest -o orderer.hypertest.com:7050
 
+cd hlf_2.2_docker_free/chaincode/packaging
+tar cfz code.tar.gz connection.json
+tar cfz marbles-org1.tgz code.tar.gz metadata.json
+peer lifecycle chaincode install marbles-org1.tgz
+
 peer lifecycle chaincode install marbles-org1.tgz
 peer lifecycle chaincode queryinstalled
+
+peer lifecycle chaincode approveformyorg --channelID hypertest --name marbles --version 1.0 --init-required --package-id marbles:d8140fbc1a0903bd88611a96c5b0077a2fdeef00a95c05bfe52e207f5f9ab79d --sequence 1 -o orderer.hypertest.com:7050
+
+peer lifecycle chaincode checkcommitreadiness --channelID hypertest --name marbles --version 1.0 --init-required --sequence 1 -o orderer.hypertest.com:7050 
+
+peer lifecycle chaincode commit -o orderer.hypertest.com:7050 --channelID hypertest --name marbles --version 1.0 --sequence 1 --init-required --peerAddresses peer0.org1.hypertest.com:7051
+
